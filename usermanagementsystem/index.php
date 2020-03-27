87f5124b012dd8f264a23508fed1864abbe3d81a
subahanumanth <55708117+subahanumanth@users.noreply.github.com>
2020-03-27 11:12:18 +0530

    Delete index.php

--- usermanagementsystem/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$url = $_GET['url'];
-$url = explode('/',$url);
-if(isset($url[0])) {
-  require("./controller/index.php");
-  $controller = new index ();
-  $controller->{$url[0]} ();
-}
-?>
